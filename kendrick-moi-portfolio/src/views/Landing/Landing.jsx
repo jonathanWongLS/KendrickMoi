039dc43b4e35073e21c713f4ed139a47346e0cf3
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-01-31 04:22:38 +0800

    Included Landing page video, title and subtitle

--- kendrick-moi-portfolio/src/views/Landing/Landing.jsx
+++ kendrick-moi-portfolio/src/views/Landing/Landing.jsx
@@ -1,8 +1,27 @@
+import {Helmet} from "react-helmet";
+import landingVideo from "./assets/SABAH.mp4";
 import "./Landing.css";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className="landing-container">
+      <Helmet>
+        <body style="overflow: hidden" />
+      </Helmet>
+      <video width="100%" height="100%" muted autoPlay loop className="landing-video">
+        <source src={landingVideo} type="video/mp4" />
+      </video>
+      <div className="landing-content-container">
+        <div className="landing-top-right-container">
+        </div>
+        <div className="landing-center-container">
+          <p className="landing-title">KENDRICK MOI</p>
+          <a href="" target="_blank" className="landing-subtitle">
+            BRINGING IDEAS TO LIFE
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
