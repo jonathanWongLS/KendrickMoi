355dd5f519c715dd27519953416dac1610bf7633
JonWLS <75321066+jonathanWongLS@users.noreply.github.com>
2024-01-31 05:01:53 +0800

    Implemented social media icon links and portfolio link at the top right section of the container

--- kendrick-moi-portfolio/src/views/Landing/Landing.jsx
+++ kendrick-moi-portfolio/src/views/Landing/Landing.jsx
@@ -1,4 +1,9 @@
 import {Helmet} from "react-helmet";
+
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+
 import landingVideo from "./assets/SABAH.mp4";
 import "./Landing.css";
 
@@ -13,10 +18,24 @@ const Landing = () => {
       </video>
       <div className="landing-content-container">
         <div className="landing-top-right-container">
+          <div className="socmed-links-container">
+            <a href="https://www.instagram.com/kendrickmoi_/" rel="noreferrer noopener" target="_blank" className="socmed-link">
+              <FaInstagram /> 
+            </a>
+            <a href="https://www.facebook.com/kendrick.moi" rel="noreferrer noopener" target="_blank" className="socmed-link">
+              <FaFacebook />
+            </a>
+            <a href="mailto:jonathanwongls01@gmail.com" rel="noreferrer noopener" target="_blank" className="socmed-link">
+              <AiOutlineMail />
+            </a>
+          </div>
+          <div className="portfolio-link-container">
+            <a href="/portfolio" target="_blank" className="portfolio-link">PORTFOLIO</a>
+          </div>
         </div>
         <div className="landing-center-container">
           <p className="landing-title">KENDRICK MOI</p>
-          <a href="" target="_blank" className="landing-subtitle">
+          <a href="/portfolio" target="_blank" className="landing-subtitle">
             BRINGING IDEAS TO LIFE
           </a>
         </div>
