cf8c341d867521cfa9c891db5469d3f317423662
JonathanWongLS <jonathanwongls01@gmail.com>
2024-02-01 16:05:22 +0800

    Created Portfolio component's initial react component

---
+++ src/views/Portfolio/Portfolio.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+import "./Portfolio.css";
+
+const Portfolio = () => {
+  useEffect(() => document.title = "Kendrick Moi | Portfolio");
+  
+  return (
+    <div>Portfolio</div>
+  )
+}
+
+export default Portfolio
