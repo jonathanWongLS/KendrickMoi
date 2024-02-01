2dcce7f25fb9af6400dad819474d6adfd4f5c65d
JonathanWongLS <jonathanwongls01@gmail.com>
2024-02-01 17:01:04 +0800

    Implemented title and page links with underline animations

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -6,7 +6,24 @@ const Portfolio = () => {
   useEffect(() => document.title = "Kendrick Moi | Portfolio");
   
   return (
-    <div>Portfolio</div>
+    <div className="portfolio-main-container">
+      <div className="portfolio-menu-mobile">Menu</div>
+      <div className="portfolio-title">
+        <a href="/" rel="noreferrer noopener" className="portfolio-title-anchor">
+          <h1>KENDRICK MOI</h1>
+        </a>
+      </div> 
+      <div className="portfolio-links">
+        <a href="/about" rel="noreferrer">
+          <span>About</span>
+        </a>
+        <a href="/portfolio" rel="noreferrer" className="active">
+          <span>Portfolio</span>
+        </a>
+      </div>
+      <div className="portfolio-items">Items</div>
+      <div className="portfolio-socmed-links">Socmed Links</div>
+    </div>
   )
 }
 
